cffeab54672ec8468dc6f6163086f245c32e22e1
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-12 13:31:37 +0530

    Added pathc route for post

--- index.mjs
+++ index.mjs
@@ -11,6 +11,9 @@ app.use(bodyParser.json({ limit : "30mb", extended : true}));
 app.use(bodyParser.urlencoded({ limit : "30mb", extended : true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
+
 const CONNECTION_URL = 'mongodb+srv://sadiq-1698:Iamtheboss1698@todo.rodyv.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
@@ -26,4 +29,3 @@ mongoose.connect(
 .catch((error) => console.log(error.message));
 
 
-app.use('/posts', postRoutes);
--- routes/posts.mjs
+++ routes/posts.mjs
@@ -5,6 +5,7 @@ import { getPosts, createPost } from '../controllers/posts.mjs';
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
