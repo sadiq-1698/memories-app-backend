9399b287b582dff3390db559cc583ee1a6629912
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-12 13:37:18 +0530

    Added controller for update post route

--- controllers/posts.mjs
+++ controllers/posts.mjs
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.mjs';
+import { Mongoose } from 'mongoose';
 
 export const getPosts =  async (req, res) => {
     try {
@@ -21,4 +22,17 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({message : error.message});
     }
+}
+
+export const updatePost = async (req, res) => {
+
+    const {id : _id } = req.params;
+
+    if(!Mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new : true});
+
+    res.json(updatedPost);
+
 }
