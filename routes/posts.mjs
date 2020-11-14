4951b7a2db796a7a53de1e960c44e30cba15b797
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-14 11:38:26 +0530

    added likePost route

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.mjs';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.mjs';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.delete('/:id/likePost', likePost);
 
 export default router;
