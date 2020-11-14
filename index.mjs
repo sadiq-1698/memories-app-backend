25fc87d4f766be0c3dccb2cb6a9324a2f30e5e35
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-14 12:32:00 +0530

    Project complete

--- index.mjs
+++ index.mjs
@@ -2,23 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.mjs';
 
 
 const app = express();
+dotenv.config();
+
 app.use(bodyParser.json({ limit : "30mb", extended : true}));
 app.use(bodyParser.urlencoded({ limit : "30mb", extended : true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-
-const CONNECTION_URL = 'mongodb+srv://sadiq-1698:Iamtheboss1698@todo.rodyv.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(
-   CONNECTION_URL, {
+   process.env.CONNECTION_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
